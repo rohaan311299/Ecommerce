0fc1e43a62f94854757fdabfa7284e170282bc93
Rohan Kacheria <rohankacheriam@gmail.com>
2022-06-15 11:09:43 +0530

    new changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
 
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar className="header"  variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
             <Navbar.Brand>Company Name</Navbar.Brand>
